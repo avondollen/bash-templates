eefe3ad5e84305584d5d279f0b9ae6817d5cde8c
Alexander Daniel von Dollen <alexvondollen@live.com>
2024-06-29 16:05:29 -0400

    simplify logger function

--- logger.bash
+++ logger.bash
@@ -1,62 +1,67 @@
-#!/bin/bash
+#!/bin/env bash
+
+# Formatting
+declare -r RESET_FORMATTING="\x1B[0m"
+declare -r BOLD="\x1B[1m"
+declare -r FAINT="\x1B[2m"
+
+# Font Color Codes
+declare -r RED_TEXT="\x1B[31m"
+declare -r GREEN_TEXT="\x1B[32m"
+declare -r BLUE_TEXT="\x1B[34m"
+
+# Background Color Codes
+declare -r BLACK_BACKGROUND="\x1B[40m"
 
 # Basic colorized logging method.
 # If environment variable LS_COLORS is not set, color formatting is not used.
 logger ()
 {
-  if [ "$LS_COLORS" ]; then
-    local logging_color
-    
+  if [ "$LS_COLORS" ]; then    
     case $1 in
-      # Foreground Process - Terminal default "Foreground Text" color
-      "base_proc")
-        logging_color=$(awk '{ sub(/.*rs=/, ""); sub(/:.*/, "");print }' <<< "${LS_COLORS}")
+      INFO)
+        printf "${RESET_FORMATTING}%s\n" "$2"
         ;;
-      # Child Processes - Format code for faint/dim text '[2m'
-      "child_proc")
-        logging_color=2
+      CHILD)
+        printf "${FAINT}%s${RESET_FORMATTING}\n" "$2"
         ;;
-      # Success - Terminal Color Scheme for executable Files
-      "success")
-        logging_color=$(awk '{ sub(/.*:ex=/, ""); sub(/:.*/, "");print }' <<< "${LS_COLORS}")
+      SUCCESS)
+        printf "${BOLD}${GREEN_TEXT}%s${RESET_FORMATTING}\n" "$2"
         ;;
-      # Error - Terminal Color Scheme for Orphan Symlinks
-      "error")
-        logging_color=$(awk '{ sub(/.*:or=/, ""); sub(/:.*/, "");print }' <<< "${LS_COLORS}")
+      ERROR)
+        printf "${BOLD}${RED_TEXT}${BLACK_BACKGROUND}%s${RESET_FORMATTING}\n" "$2"
         ;;
-      "debug")
-        logging_color=$(awk '{ sub(/.*:di=/, ""); sub(/:.*/, "");print }' <<< "${LS_COLORS}")
+      DEBUG)
+        printf "${BOLD}${BLUE_TEXT}%s${RESET_FORMATTING}\n" "$2"
         ;;
       *)
-        logger "error" "[Function:Logger]: invalid param: '$1'"
+        logger ERROR "[Function:Logger]: invalid param: '$1'"
         printf "\n"
         exit 1
         ;;
     esac
-
-  printf "\e[%sm%s\e[0m \n" "${logging_color}" "$2"
-
   else
     printf "%s\n" "$2"
   fi
 }
 
-logger "base_proc" "--------------------------------------------"
-logger "base_proc" "   B A S H    L O G G I N G    D E M O      "
-logger "base_proc" "--------------------------------------------"
-logger "base_proc" "Base proccess doing things."
-logger "child_proc" "Sub proccess doing things 1/3....."
-logger "child_proc" "Sub proccess doing things 2/3....."
-logger "child_proc" "Sub proccess doing things 3/3....."
-logger "success" "Sub proccess executed successfully."
-logger "base_proc" "Printing Distro Description"
-logger "child_proc" "$(lsb_release -d)"
-logger "error" "Exit-Code: 2 - error example"
-logger "debug" "debugging example"
-logger "base_proc" "--------------------------------------------"
+
+logger INFO    "--------------------------------------------"
+logger INFO    "   B A S H    L O G G I N G    D E M O      "
+logger INFO    "--------------------------------------------"
+logger INFO    "Base proccess doing things."
+logger CHILD   "Sub proccess doing things 1/3....."
+logger CHILD   "Sub proccess doing things 2/3....."
+logger CHILD   "Sub proccess doing things 3/3....."
+logger SUCCESS "Sub proccess executed successfully."
+logger INFO    "Printing Distro Description"
+logger CHILD   "$(lsb_release -d)"
+logger ERROR   "Exit-Code: 2 - error example"
+logger DEBUG   "debugging example"
+logger INFO    "--------------------------------------------"
 printf "\n"
-logger "base_proc" "Showing loggers ability to handle bad inputs"
+logger INFO "Showing loggers ability to handle bad inputs"
 logger "goofed" "Lets break the logger function."
 
 # Note: As  long as line 59 is present nothing else below will execute!
-logger "base_proc" "This will only be printed in line 59 is commented out/removed!"
+logger INFO "This will only be printed in line 59 is commented out/removed!"
